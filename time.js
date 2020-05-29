a56b30bd83a844dca4a796746d7459c0e871d97e
cyril <cyrilwang012@gmail.com>
2020-05-29 01:07:35 -0700

    animation update

--- time.js
+++ time.js
@@ -14,11 +14,35 @@ var endTime = y + 1 + '-01-01 00:00:00:000',
     endDate = new Date(endTime),
     endDate = new Date(endTime.replace(/-/g, '/'));
 
-pervTime = pervDate.getTime();
-currentTime = d.getTime();
-endTime = endDate.getTime();
+// var pervTime = pervDate.getTime(),
+//     currentTime = d.getTime(),
+//     endTime = endDate.getTime();
 
 // document.getElementById('test').innerHTML = (currentTime - pervTime) / (endTime - pervTime);
+// document.getElementById('fill').style.width = (currentTime - pervTime) / (endTime - pervTime) * 1500 + 'px';
 
-document.getElementById('fill').style.width = (currentTime - pervTime) / (endTime - pervTime) * 1500 + 'px';
+function init(){
+    var pervTime = pervDate.getTime(),
+        currentTime = d.getTime(),
+        endTime = endDate.getTime(),
+        fillWidth = (currentTime - pervTime) / (endTime - pervTime) * 1500,
+        fill = document.getElementById('fill'),
+        i = 0,
+        animation = setInterval(frame, 10);
+
+    function frame(){
+        if(i == Math.round(fillWidth)){
+            clearInterval(animation);
+        }else{
+            i++;
+            fill.style.width = i + 'px';
+        }
+    }
+        
+}
+
+window.onload = init();
+
+
+
 
